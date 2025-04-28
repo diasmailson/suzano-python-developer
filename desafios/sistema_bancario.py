ed855031ce2d1ba2fa4ac3cde8df1bd6bc6736ce
mailsondias.dev <mailsondias.dev@gmail.com>
2025-04-28 11:36:43 -0300

    feat: Adicionando  opção de saque

--- desafios/sistema_bancario.py
+++ desafios/sistema_bancario.py
@@ -29,7 +29,28 @@ while True:
             print("Srª Cliente, insira valores maior que zero.")
 
     elif opcao == '2':  # Sacar
-        pass
+        EXCEDEU_SAQUE = numero_saques < LIMITE_SAQUE
+
+        valor_saque = float(input("Informe o valor de Saque: R$ "))
+
+        if valor_saque > float(500):
+            print(
+                "O valor máximo permitido para saque é de R$ 500.00. Por favor, insira outro valor.")
+
+        elif valor_saque > saldo:
+            print(
+                "Sr(a) Cliente, não será possível sacar o dinheiro por falta de saldo na conta.")
+
+        elif valor_saque > 0 and EXCEDEU_SAQUE:
+            saldo -= valor_saque
+            extrato += f"Saque: R$ {valor_saque:.2f}\n"
+            numero_saques += 1
+
+        elif numero_saques > EXCEDEU_SAQUE:
+            print("Número de saques excedidos. São apenas 3 saques diários.")
+
+        else:
+            print("Valor inválido. Insira um valor maior que zero.")
 
     elif opcao == '3':  # Extrato
         pass
