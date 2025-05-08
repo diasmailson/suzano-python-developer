581acff1236e8fce62ce51644996ddd79668d93d
mailsondias.dev <mailsondias.dev@gmail.com>
2025-05-08 18:56:22 -0300

    feat: Adicionando função sacar

--- desafios/sistema_bancario_v2/sistema_bancario_v2.py
+++ desafios/sistema_bancario_v2/sistema_bancario_v2.py
@@ -23,3 +23,22 @@ def depositar(saldo, valor, extrato, /):
     else:
         print("\n @@@Valor inválido. Insira um valor maior que zero. @@@")
     return saldo, extrato
+
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saque):
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saques = numero_saques >= limite_saque
+
+    if excedeu_saldo:
+        print("\n @@@ Saldo insuficiente! @@@")
+    elif excedeu_limite:
+        print("\n @@@ O valor máximo para saque é de R$ 500.00 @@@")
+    elif excedeu_saques:
+        print("\n @@@ Número máximo de saques diários atingido! @@@")
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque:\t\tR$ {valor:.2f}\n"
+        numero_saques += 1  
+    else:
+        print("\n @@@ Valor inválido. Insira um valor maior que zero. @@@")
+    return saldo, extrato 
