97d73bda651179260cfdc02b1dffc460d275a485
mailsondias.dev <mailsondias.dev@gmail.com>
2025-05-08 18:52:37 -0300

    feat: Adicionando menu

---
+++ desafios/sistema_bancario_v2/sistema_bancario_v2.py
@@ -0,0 +1,16 @@
+import textwrap
+
+def menu():
+    menu = """
+================ MENU ================
+    [d]\tDepositar
+    [s]\tSacar
+    [e]\tExtrato
+    [nc]\tNova conta
+    [lc]\tListar contas
+    [nu]\tNovo usuário
+    [q]\tSair
+    => """
+    return input(textwrap.dedent(menu))
+
+
